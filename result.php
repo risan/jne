6e01183f5b9b3ba59d0dd8274c2821e42def8b63
Risan Bagja <risanbagja@yahoo.com>
2014-01-18 14:09:21 +0700

    Add CSS rules for result table

--- result.php
+++ result.php
@@ -9,4 +9,53 @@ $jne = new JNE();
 // Get the response
 $response = $jne->getResponse();
 // Parse response
-$jne->parseResponse($response);
+$data = $jne->parseResponse($response);
+$header = $data['header'];
+$content = $data['content'];
+?><!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>JNE Tariff Scraper</title>
+        <link rel="stylesheet" href="css/styles.css">
+    </head>
+    <body>
+        <div class="container">
+            <table class="header">
+                <tbody>
+                    <tr>
+                        <th>From</th>
+                        <td><?php echo $header['from'] ;?></td>
+                    </tr>
+                    <tr>
+                        <th>To</th>
+                        <td><?php echo $header['to'] ;?></td>
+                    </tr>
+                    <tr>
+                        <th>Weight (kgs)</th>
+                        <td><?php echo $header['weight'] ;?></td>
+                    </tr>
+                </tbody>
+            </table>
+
+            <table class="content">
+                <thead>
+                    <tr>
+                        <?php foreach ($header['header'] as $field): ?>
+                            <th><?php echo $field; ?></th>
+                        <?php endforeach; ?>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($content as $row): ?>
+                        <tr>
+                            <?php foreach ($row as $col): ?>
+                                <td><?php echo $col;?></td>
+                            <?php endforeach; ?>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+    </body>
+</html>
